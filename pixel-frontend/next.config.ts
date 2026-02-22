348fdc4b37472e67ea9fc90f8e5750d3c44690af
PixelStudio330 <epicmoth14@gmail.com>
2026-02-22 14:09:35 +0600

    varcel redeployment

--- pixel-frontend/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // ⚡ ignore all ESLint errors on Vercel build
-  },
-};
-
-export default nextConfig;
